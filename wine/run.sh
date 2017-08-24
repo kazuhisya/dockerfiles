9b7b8c5a6143173bcb86adbb47ab73f1b64f104d
Kazuhisa Hara <kazuhisya@gmail.com>
2017-08-24 10:36:40 +0900

    wine: "--device" option can be used instead of "-v /dev/snd..."
    As of Docker 1.8 or higher, the "--device" option can be used instead
    of "-v /dev/snd:/dev/snd --privileged". it useful.

--- wine/run.sh
+++ wine/run.sh
@@ -7,7 +7,7 @@ docker run -ti --rm \
   -e LC_CTYPE=ja_JP.UTF-8 \
   -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   -v ~/.Xauthority:/home/wine/.Xauthority \
-  -v /dev/snd:/dev/snd --privileged \
+  --device /dev/snd \
   -v /run/user/`id -u`/pulse/native:/run/user/`id -u`/pulse/native \
   -v `pwd`/share:/home/wine/shared_directory \
   local/wine:base
