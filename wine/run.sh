6504b4617b8873e12898446787e9fddb11958d5a
Kazuhisa Hara <kazuhisya@gmail.com>
2017-08-24 10:25:54 +0900

    wine: changed user name and misc

--- wine/run.sh
+++ wine/run.sh
@@ -5,5 +5,9 @@ docker run -ti --rm \
   -e QT_IM_MODULE=ibus \
   -e XIMPROGRAM=ibus \
   -e LC_CTYPE=ja_JP.UTF-8 \
-  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -v ~/.Xauthority:/home/wine/.Xauthority \
+  -v /dev/snd:/dev/snd --privileged \
+  -v /run/user/`id -u`/pulse/native:/run/user/`id -u`/pulse/native \
+  -v `pwd`/share:/home/wine/shared_directory \
   local/wine:base
