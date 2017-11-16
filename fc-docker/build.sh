21474b1a1a2a9aaf035b0207c2aa458c7827de21
Kazuhisa Hara <kazuhisya@gmail.com>
2017-11-16 10:25:22 +0900

    fc-docker: update docker base image to fc27

--- fc-docker/build.sh
+++ fc-docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker rmi $(docker images local/fc26 -q)
+docker rmi $(docker images local/fc27 -q)
 for name in dev-tools
 do
-    docker build -t local/fc26:${name} -f ./Dockerfile-${name} .
+    docker build -t local/fc27:${name} -f ./Dockerfile-${name} .
 done
