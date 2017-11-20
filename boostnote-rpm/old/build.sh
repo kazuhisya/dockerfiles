786a09004eee32eec9d4b982a842a16a655db4bb
Kazuhisa Hara <kazuhisya@gmail.com>
2017-11-20 11:18:09 +0900

    boostnote: Build method synchronized with upstream

--- boostnote-rpm/old/build.sh
+++ boostnote-rpm/old/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 time docker build -t local/boostnote .
 docker run -it --name boostnote local/boostnote true
-docker cp boostnote:/boostnote/dist/ .
+docker cp boostnote:/home/mock/rpmbuild/RPMS/x86_64 .
 docker rm boostnote
