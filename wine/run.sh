edce3d32f752c58f04749db47ab07049b3694967
Kazuhisa Hara <kazuhisya@gmail.com>
2020-04-29 22:55:22 +0900

    wine: update docker base image to fc32 and misc

--- wine/run.sh
+++ wine/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# podman or docker-ce
+# podman needs --userns=keep-id
+if [[ -e /usr/bin/podman ]]; then
+  KEEPID="--userns=keep-id"
+fi
+
 # selinux label
 sudo chcon -Rt svirt_sandbox_file_t ./share
 
 # docker run
 docker run -ti --rm \
+  ${KEEPID} \
   -e XMODIFIERS="@im=ibus" \
   -e GTK_IM_MODULE=ibus \
   -e QT_IM_MODULE=ibus \
@@ -12,7 +19,6 @@ docker run -ti --rm \
   -e LC_CTYPE=ja_JP.UTF-8 \
   -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   --user=$(id -u):$(id -g) \
-  --userns=keep-id \
   --security-opt label=type:container_runtime_t \
   --device /dev/snd \
   -v /run/user/`id -u`/pulse/native:/run/user/`id -u`/pulse/native:z \
